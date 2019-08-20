0d047cbe4ceeebb6d2400a862f60990f5671691a
jankleinert <jkleiner@redhat.com>
2019-08-20 11:15:09 -0400

    Adding additional content

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,6 @@
 WORKSHOP_NAME=homeroom-starter
 WORKSHOP_IMAGE=quay.io/jkleiner/homeroom-starter-guide
+SPAWNER_MODE=hosted-workshop
 RESOURCE_BUDGET=medium
-MAX_SESSION_AGE=3600
-IDLE_TIMEOUT=300
+MAX_SESSION_AGE=10800
+IDLE_TIMEOUT=600
